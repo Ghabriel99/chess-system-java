9cf9ebbca3c54138c0c8ab85f0e6ae60749c730f
Ghabriel Marchesi Schiavo <86672683+Ghabriel99@users.noreply.github.com>
2023-03-02 14:38:25 -0300

    .
    BoardException and Defensive programming

--- application/UI.java
+++ application/UI.java
@@ -7,14 +7,14 @@ public class UI {
    public static void printBoard(ChessPiece[][] pieces) {
       
       for (int i=0; i<8; i++) {
-         System.out.println((8 - i) + " ");
+         System.out.print((8 - i) + " ");
 
          for (int j=0; j<pieces.length; j++) {
             printPiece(pieces[i][j]);
          }
          System.out.println(" ");
       }
-      System.out.println(" a b c d e f g h");
+      System.out.println("  a b c d e f g h");
    }
 
    private static void printPiece(ChessPiece piece) {
@@ -23,6 +23,6 @@ public class UI {
       } else {
          System.out.print(piece);
       }
-      System.out.println(" ");
+      System.out.print(" ");
    }
 }
